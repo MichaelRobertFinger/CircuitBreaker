ed8fa3fcafade2e27a6ce7d5c6b8bc27a15b54b5
aardvarkads <mike@michaelfinger.com>
2013-11-16 09:27:01 -0500

    retry extension in progress

---
+++ CircuitBreaker.Test/CircuitBreakerExtensionTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CircuitBreaker.Test
+{
+    [TestClass]
+    public class CircuitBreakerExtensionTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(OperationFailedException))]
+        public void Retry_Failure()
+        {
+            var func = new Func<int>(() => { throw new Exception(); });
+            var cb = new CircuitBreaker(500, 1);
+            cb.Retry<int>(func, 2);
+
+
+        }
+    }
+}
